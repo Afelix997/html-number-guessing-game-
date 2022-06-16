fb270d2f1c6d3e2fe163ae3e462fd18d8f6f2cc2
afelix997 <afelix2013@gmail.com>
2022-06-16 06:45:34 -0700

    working

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 console.log("HELLO PAPA PLATOON!")
 // Your function(s) should go here that will interact with the webpage or DOM
+num = Math.floor(Math.random() * 100) + 1;
 let randomNumber = function() {
     result=''
-    num = Math.floor(Math.random() * 100) + 1;
+    console.log(num)
     
     var userInput = document.getElementById("userInput").value;
     if( userInput == num ){
